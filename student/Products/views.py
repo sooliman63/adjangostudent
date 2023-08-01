d317d87697cf687ba4ac4d1af0e3a90630dcf99c
sooliman63 <137851776+Sooliman122@users.noreply.github.com>
2023-08-02 01:12:08 +0300

    finesh corse django 103

--- student/Products/views.py
+++ student/Products/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-
+from django.core.paginator import Paginator
+from .models import Product,Seller
 # Create your views here.
 def home(request):
     return render(request, 'products/home.html')
 def products(request):
-    return render(request, 'products/products.html')
+    products= Product.objects.all()
+    paginator = Paginator(products,2)
+    page =request.GET.get('page')
+    paged_produts =paginator.get_page(page) 
+    context={'products':paged_produts}
+    return render(request, 'products/products.html',context)
